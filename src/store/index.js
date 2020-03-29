ea7477c71e581b33f1ae633c4c6b691fe995af5a
Aruga <arugatatsuya@aruga.local>
2020-03-29 16:47:18 +0900

    ADD submit form

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,50 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
+const savedLists = localStorage.getItem('trello-lists')// ★①追加
+
+
+const store = new Vuex.Store({// ★②編集
+ state: {
+  lists: savedLists ? JSON.parse(savedLists): [
+      {
+        title: 'Backlog',
+        cards: [
+          { body: 'English' },
+          { body: 'Mathematics' },
+        ]
+      },
+      {
+        title: 'Todo',
+        cards: [
+          { body: 'Science' }
+        ]
+      },
+      {
+        title: 'Doing',
+        cards: []
+      }
+    ],
+   // ★ここまで③編集
   },
   mutations: {
+    addlist(state, payload) {
+      state.lists.push({ title: payload.title, cards:[] })
+    },
   },
   actions: {
+    addlist(context, payload) {
+      context.commit('addlist', payload)
+    },
   },
-  modules: {
+  getters: {
   }
 })
+
+// ★ここから追記
+store.subscribe((mutation, state) => {
+  localStorage.setItem('trello-lists', JSON.stringify(state.lists))
+})
+
+export default store
+// ★ここまで追記
